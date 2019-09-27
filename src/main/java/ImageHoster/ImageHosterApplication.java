2558bc27aeb0a1e4bd0273a2871fc8cb7fe5c8fd
avia-fernandes <avia.fernandes@gmail.com>
2019-09-27 12:46:42 +0300

    Part A completed

--- src/main/java/ImageHoster/ImageHosterApplication.java
+++ src/main/java/ImageHoster/ImageHosterApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+//ASSIGNMENT SUBMISSION BY AVIA FERNANDES
 @SpringBootApplication
 public class ImageHosterApplication {
     public static void main(String[] args) {
--- src/main/java/ImageHoster/service/ImageService.java
+++ src/main/java/ImageHoster/service/ImageService.java
@@ -27,7 +27,7 @@ public class ImageService {
 
 
     //The method calls the getImageByTitle() method in the Repository and passes the title of the image to be fetched
-    public Image getImageByTitle(String title) {
+    public Image getImageByTitle(Integer imageId, String title) {
         return imageRepository.getImageByTitle(title);
     }
 
